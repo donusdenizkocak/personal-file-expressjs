0abd2a141ff98c992aa8897bfb009fa25f0334b2
denizderK <denizderya8668@gmail.com>
2023-11-20 00:34:13 +0300

    edt

--- src/index.ts
+++ src/index.ts
@@ -32,11 +32,11 @@ AppDataSource.initialize().then(async () => {
     // ...
 
     // start express server
-    app.listen(3000)
+    app.listen(3080)
 
     // insert new users for test
    
 
-    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results")
+    console.log("Express server has started on port 3000. Open http://localhost:3080/users to see results")
 
 }).catch(error => console.log(error))
